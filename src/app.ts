04aa9ee6adaf30c213bfcd84a7cd176929942d85
Laka Nodia <112949971+Lakanno@users.noreply.github.com>
2025-04-06 22:24:52 +0400

    Add material UI

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,34 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views")); // 📁 folder for ejs templates
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the API!");
+  // res.send("Welcome to the API!");
+  res.redirect("/login");
+  // res.render('home',  { name: 'Friend' });
 });
 
 // მომხმარებლების როუტები
